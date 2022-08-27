37d1421d21d4fe68fd2e88188df12dd0e0d147c6
Glsme <glasses.str.man@gmail.com>
2022-08-27 18:05:12 +0900

    Feat: Naver API 관련 파일 생성

---
+++ ShoppingTodoApp/APIManager/NaverAPIManager.swift
@@ -0,0 +1,14 @@
+//
+//  NaverAPIManager.swift
+//  ShoppingTodoApp
+//
+//  Created by Seokjune Hong on 2022/08/27.
+//
+
+import Foundation
+
+class NaverAPIManager {
+    static let shared = NaverAPIManager()
+    
+    private init() { }
+}
