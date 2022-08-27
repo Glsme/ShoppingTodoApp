d208ded44e5cf8c861fa91a703202608472c63e9
Glsme <glasses.str.man@gmail.com>
2022-08-27 16:19:12 +0900

    Feat: 화면 전환을 위한 UIViewController Extension 파일 및 코드 추가

---
+++ ShoppingTodoApp/Extension/UIViewController+Extension.swift
@@ -0,0 +1,34 @@
+//
+//  UIViewController+Extension.swift
+//  ShoppingTodoApp
+//
+//  Created by Seokjune Hong on 2022/08/27.
+//
+
+import UIKit
+
+extension UIViewController {
+    enum TransitionType {
+        case push
+        case present
+        case presentNavigation
+        case presentFullScreenNavigation
+    }
+    
+    ///Trans ViewController: Animation Default true
+    func transViewController<T: UIViewController>(ViewController vc: T,transitionType: TransitionType) {
+        switch transitionType {
+        case .push:
+            navigationController?.pushViewController(vc, animated: true)
+        case .present:
+            present(vc, animated: true)
+        case .presentNavigation:
+            let navi = UINavigationController(rootViewController: vc)
+            present(navi, animated: true)
+        case .presentFullScreenNavigation:
+            let navi = UINavigationController(rootViewController: vc)
+            navi.modalPresentationStyle = .fullScreen
+            navigationController?.pushViewController(navi, animated: true)
+        }
+    }
+}
