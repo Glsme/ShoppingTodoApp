973d843d634c7b9c30aa5b2c83c89016b716d1af
Glsme <glasses.str.man@gmail.com>
2022-08-27 18:11:52 +0900

    Feat: NaverAPIManager 내 이미지 검색 관련 네트워크 통신 코드 추가

--- ShoppingTodoApp/APIManager/NaverAPIManager.swift
+++ ShoppingTodoApp/APIManager/NaverAPIManager.swift
@@ -6,10 +6,36 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
 
 class NaverAPIManager {
     static let shared = NaverAPIManager()
     
     private init() { }
     
+    private let header: HTTPHeaders = ["X-Naver-Client-Id": APIKey.NAVER_ID, "X-Naver-Client-Secret": APIKey.NAVER_SECRET]
+    
+    typealias completionHandler = (Int, [String]) -> Void
+    
+    func callRequest(query: String, startPage: Int, completionHandler: @escaping completionHandler ) {
+        let text = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        let url = "\(EndPoint.naverSearchImageURL)query=\(text)&display=30&start=\(startPage)"
+        
+        AF.request(url, method: .get, headers: header).validate(statusCode: 200...500).responseData(queue: .global()) { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+//                print(json)
+                
+                let totalCount = json["total"].intValue
+                let list = json["items"].arrayValue.map{ $0["link"].stringValue }
+                
+                completionHandler(totalCount, list)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
---
+++ ShoppingTodoApp/Key/EndPoint.swift
@@ -0,0 +1,12 @@
+//
+//  EndPoint.swift
+//  ShoppingTodoApp
+//
+//  Created by Seokjune Hong on 2022/08/27.
+//
+
+import Foundation
+
+struct EndPoint {
+    static let naverSearchImageURL = "https://openapi.naver.com/v1/search/image.json?"
+}
