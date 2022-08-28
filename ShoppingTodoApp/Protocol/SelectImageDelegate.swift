d764dcad2626fb7d0e0bdd32c51bca368333cb05
Glsme <glasses.str.man@gmail.com>
2022-08-28 23:28:21 +0900

    Feat: SelectImageDelegate Protocol 파일 생성

---
+++ ShoppingTodoApp/Protocol/SelectImageDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SelectImageDelegate.swift
+//  ShoppingTodoApp
+//
+//  Created by Seokjune Hong on 2022/08/28.
+//
+
+import UIKit
+
+protocol SelectImageDelegate {
+    func sendImageData(image: UIImage)
+}
